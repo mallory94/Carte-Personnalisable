9f879d647b87014990001dbe614a97dbaed2f24e
mallory94 <malloga@hotmail.fr>
2020-03-25 01:38:38 +0100

    Mise à jour de la base de données

--- pwebcbdd.sql
+++ pwebcbdd.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.4
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  mar. 24 mars 2020 à 00:52
--- Version du serveur :  5.7.24
--- Version de PHP :  7.2.14
+-- Généré le :  mer. 25 mars 2020 à 00:29
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -34,7 +34,7 @@ CREATE TABLE IF NOT EXISTS `carte` (
   `LabelCarte` varchar(100) NOT NULL,
   `idUtilisateur` int(11) NOT NULL,
   PRIMARY KEY (`idCarte`)
-) ENGINE=MyISAM AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=6 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `carte`
@@ -43,7 +43,8 @@ CREATE TABLE IF NOT EXISTS `carte` (
 INSERT INTO `carte` (`idCarte`, `LabelCarte`, `idUtilisateur`) VALUES
 (1, 'CarteTransports', 6),
 (3, 'RoadMap', 5),
-(4, 'NomCarte', 6);
+(4, 'NomCarte', 6),
+(5, 'NomCarte', 7);
 
 -- --------------------------------------------------------
 
@@ -83,14 +84,20 @@ CREATE TABLE IF NOT EXISTS `marqueurs` (
   `iconUrl` varchar(100) NOT NULL,
   PRIMARY KEY (`idMarqueur`),
   KEY `FK_IdCarteMarqueurs` (`idCarte`)
-) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
+) ENGINE=MyISAM AUTO_INCREMENT=25 DEFAULT CHARSET=latin1;
 
 --
 -- Déchargement des données de la table `marqueurs`
 --
 
 INSERT INTO `marqueurs` (`idMarqueur`, `LabelMarqueur`, `LattitudeMarqueur`, `LongitudeMarqueur`, `idCarte`, `iconUrl`) VALUES
-(1, 'CentreParis', 48.855599, 2.349101, 1, '');
+(23, NULL, 48.864940612458, 2.2446441650391, 1, 'https://img.icons8.com/clouds/100/000000/cloud-network.png'),
+(22, NULL, 48.833085609348, 2.4269485473633, 1, 'https://img.icons8.com/clouds/100/000000/cloud-network.png'),
+(18, NULL, 48.867199062954, 2.1694564819336, 3, 'https://img.icons8.com/clouds/100/000000/cloud-network.png'),
+(19, NULL, 48.863585493236, 2.2147750854492, 3, 'https://img.icons8.com/clouds/100/000000/cloud-network.png'),
+(20, NULL, 48.863133778674, 2.1965789794922, 3, 'https://img.icons8.com/clouds/100/000000/cloud-network.png'),
+(21, NULL, 48.865618158309, 2.2491073608398, 3, 'https://img.icons8.com/clouds/100/000000/cloud-network.png'),
+(24, NULL, 48.860875144709, 2.3445510864258, 1, 'https://img.icons8.com/color/64/000000/home.png');
 
 -- --------------------------------------------------------
 
@@ -132,7 +139,7 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `bConnect` tinyint(1) DEFAULT '0',
   PRIMARY KEY (`idUtilisateur`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 --
 -- Déchargement des données de la table `utilisateur`
@@ -142,7 +149,8 @@ INSERT INTO `utilisateur` (`idUtilisateur`, `pseudo`, `email`, `mdp`, `bConnect`
 (1, 'test', 'malloga@hotmail.fr', '$2y$10$IkzoKYjjO8ZSf0GFWsKol.tDPvSCvvrxtXvgQQ00XKQuiFUAbpzNW', 0),
 (3, 'adrien', 'test3@gmail.com', '$2y$10$SsS98BgHH0Ira922tjCtoOaIbABVfI6I9tOEr7GREnPYBZpI2yk5i', 0),
 (5, 'yo', 'test5@gmail.com', '$2y$10$SghX4MRBuLfuFJER60qYy.1DkUyAcVAjxQPtIXfxH3Y0.it1CzQT.', 1),
-(6, 'clem75', 'clementboutheg@gmail.com', '$2y$10$s1H8ZE58xwjTNaZrLltmyuu0uoOYwCIBQxgvfb0rL8WJEE/Bie6ty', 1);
+(6, 'clem75', 'clementboutheg@gmail.com', '$2y$10$s1H8ZE58xwjTNaZrLltmyuu0uoOYwCIBQxgvfb0rL8WJEE/Bie6ty', 1),
+(7, 'ohoh', 'ohoh@gmail.com', '$2y$10$laTRBPpigIK2CpRLf8rhU.XiZmsaaYxeuksM0PL8o/xjN6Ou5SrZO', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
